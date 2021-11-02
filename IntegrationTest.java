356a5c8242aa0bf1ebd88363bfc6d0acd5a80947
SunehaSh <89857514+SunehaSh@users.noreply.github.com>
2021-11-02 18:38:15 +0530

    Add files via upload

---
+++ IntegrationTest.java
@@ -0,0 +1,15 @@
+/**
+ * ===========================================================================
+ * Copyright &copy 2021, Brevitaz Systems, Ahmedabad, India
+ * This file and its contents are proprietary and confidential to and the sole
+ * intellectual property of Brevitaz Systems.  Any use, reproduction,
+ * redistribution or modification of this file is prohibited except as
+ * explicitly defined by written license agreement with Brevitaz Systems.
+ * ===========================================================================
+ **/
+package com.brevitaz;
+
+public class IntegrationTest
+{
+	// Write logic of your test case in this class
+}
